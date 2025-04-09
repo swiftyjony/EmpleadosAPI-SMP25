458592ab85769a95ddb28dfc90abaf4e86d148ee
Jon Gonzalez <jon.gonzalez@mo2o.com>
2025-04-09 19:48:27 +0200

    Initial Commit

---
+++ EmpleadosAPI/EmpleadosAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmpleadosAPIApp.swift
+//  EmpleadosAPI
+//
+//  Created by Jon Gonzalez on 9/4/25.
+//
+
+import SwiftUI
+
+@main
+struct EmpleadosAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
